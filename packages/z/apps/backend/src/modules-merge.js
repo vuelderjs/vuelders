dc300d5325ee6962b9a70a2dbdbc764ecce8d2ea
Alex <alex.valdiviezo@sondeos.com.ar>
2023-07-13 21:19:26 -0300

    feat(Root): Change name of repositories

--- packages/z/apps/backend/src/modules-merge.js
+++ packages/z/apps/backend/src/modules-merge.js
@@ -2,7 +2,6 @@ import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
 
 import { types as authenticationTypes, resolvers as authenticationResolvers } from '@vuelders/authorization-backend'
 //import { types as clientTypes, resolvers as clientResolvers } from './modules/clients/index.js'
-//import { types as openaiTypes, resolvers as openaiResolvers } from './modules/openai/index.js'
 
 const mergedTypes = mergeTypeDefs([authenticationTypes])
 const mergedResolvers = mergeResolvers([authenticationResolvers])
